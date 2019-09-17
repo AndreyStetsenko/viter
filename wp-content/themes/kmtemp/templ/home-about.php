8d75a129d88fc2969eae3c1e414f9bab716e0c50
Andrew Stetsenko <andrey@stetsenko.org>
2019-09-17 18:52:09 +0300

    coment home-about.php

--- wp-content/themes/kmtemp/templ/home-about.php
+++ wp-content/themes/kmtemp/templ/home-about.php
@@ -1,4 +1,4 @@
-<div class="about">
+<!-- <div class="about">
     <div class="cont_left">
         <img class="img_about" src="https://zhyt.kontramarka.ua/uploads/resize/main/420x594_show_eGghqQ0a9GXlrAcVnF-2mT8_Csb8rJer.jpg" alt="Img">
         <img class="img_about" src="https://zhyt.kontramarka.ua/uploads/resize/main/420x594_show_eGghqQ0a9GXlrAcVnF-2mT8_Csb8rJer.jpg" alt="Img">
@@ -7,4 +7,4 @@
     <div class="cont_right">
 
     </div>
-</div>
+</div> -->
